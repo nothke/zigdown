3741a549d404cf6f019a1ca28f8cd13ddf31c2ba
nothke <nothke@gmail.com>
2023-09-22 21:29:24 +0200

    Added gl and cleared color

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
 
 /// Default GLFW error handling callback
 fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
@@ -21,9 +27,18 @@ pub fn main() !void {
     };
     defer window.destroy();
 
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
     // Wait for the user to close the window.
     while (!window.shouldClose()) {
         window.swapBuffers();
+
+        gl.clearColor(1, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
         glfw.pollEvents();
     }
 }
