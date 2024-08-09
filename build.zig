2dc03b9000c6315da8b1d9807d78f115be9f6c44
nothke <nothke@gmail.com>
2024-08-09 16:50:58 +0200

    Added check step

--- build.zig
+++ build.zig
@@ -13,6 +13,54 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    addDependencies(exe, b, target, optimize);
+
+    b.installArtifact(exe);
+
+    const exe_check = b.addExecutable(.{
+        .name = "glfw-test",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    addDependencies(exe_check, b, target, optimize);
+
+    const check = b.step("check", "Check if it compiles");
+    check.dependOn(&exe_check.step);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // const unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // unit_tests.addModule("mach", @import("mach").module(mach_dep.builder, optimize, target));
+
+    // const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_unit_tests.step);
+}
+
+// This is where all the dependencies should be added
+fn addDependencies(
+    exe: *std.Build.Step.Compile,
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
     // allows @embedFile() from res
     //exe.main_pkg_path = .{ .path = "." };
 
@@ -47,30 +95,4 @@ pub fn build(b: *std.Build) void {
     exe.addCSourceFile(.{ .file = b.path("libs/cgltf.c"), .flags = &.{} });
 
     exe.addIncludePath(b.path("libs"));
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // const unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // unit_tests.addModule("mach", @import("mach").module(mach_dep.builder, optimize, target));
-
-    // const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_unit_tests.step);
 }
